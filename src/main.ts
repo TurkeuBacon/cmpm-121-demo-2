487357b0e6cdbf1e31a6a0e6c146e88a38b6a3da
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2023-10-21 13:05:17 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,9 @@ const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d")!;
 ctx.canvas.width = 256;
 ctx.canvas.height = 256;
-ctx.fillStyle = "white"
+ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 canvas.style.border = "3px solid black";
 canvas.style.borderRadius = "15px";
-canvas.style.boxShadow = "10px 10px #111111FF"
+canvas.style.boxShadow = "10px 10px #111111FF";
 app.append(canvas);
